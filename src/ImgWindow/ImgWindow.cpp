9c6237118d6a2de63a0b53c3fb29f8a8960e0b23
TwinFan <twinfan@sonux.net>
2020-07-20 22:09:36 +0200

    Avoid crash if ImgWindow::sFontAtlas is not defined
    fixes sparker256/imgui4xp#5

--- src/ImgWindow/ImgWindow.cpp
+++ src/ImgWindow/ImgWindow.cpp
@@ -118,7 +118,7 @@ ImgWindow::ImgWindow(
 	if (mFontAtlas) {
         mFontTexture = static_cast<GLuint>(reinterpret_cast<intptr_t>(io.Fonts->TexID));
     } else {
-        if (iFontAtlas->TexID == nullptr) {
+        if (!iFontAtlas || iFontAtlas->TexID == nullptr) {
             // fallback binding if an atlas wasn't explicitly set.
             unsigned char *pixels;
             int width, height;
